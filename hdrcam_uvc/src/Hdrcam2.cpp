de5bbc4a1502b6da711bc0b9875a239d14cfc783
esailly <esailly@free.fr>
2017-05-09 16:07:26 +0200

    un peu plus propre

--- hdrcam_uvc/src/Hdrcam2.cpp
+++ hdrcam_uvc/src/Hdrcam2.cpp
@@ -1,7 +1,7 @@
 /* 
  * File:   Hdrcam2.cpp
  * Author: esailly
- *
+ * Fork Code de base J Dehos
  * Created on 3 mai 2017, 13:48
  */
 
@@ -9,10 +9,10 @@
 #include <opencv2/opencv.hpp>
 #include <libuvc/libuvc.h>
 #include <mutex>
-#include <chrono>
 #include <cstdlib>
 #include <iostream>
 #include "Variables.hpp"
+#include "Chrono.hpp"
 #include <string>
 
 // uvc data (webcam)
@@ -23,18 +23,18 @@ uvc_stream_ctrl_t gCtrl;
 uvc_stream_handle_t *gStrmh;
 uvc_frame_t * gFrame;
 uint32_t aTime = 0;
+Chrono gChrono;
 
 // image parameters
 int gWidth = 640;
 int gHeight = 480;
 int imgIndex = 0;
-
-std::chrono::time_point<std::chrono::system_clock> startTime;
-std::chrono::time_point<std::chrono::system_clock> endTime;
+int imgIndex2 = 0;
 
 int cptInt = 0;
 int cptFun = 0;
-Data * gData; 
+
+Data gData; 
 
 std::mutex gMutex;
 
@@ -54,33 +54,23 @@ void tryUvc(const char * msg, uvc_error_t res) {
 void captureFun(uvc_frame_t * frame, void *) {
     // prevent concurrency (image processing)
     std::lock_guard<std::mutex> lock(gMutex);    
- 
-    if (gData->nbframe > 0 ) {
+
+    if (gData.nbframe > 0 ) {  
         // get image from webcam
         tryUvc("any2bgr", uvc_any2bgr(frame, gFrame));
-        
-//        tryUvc("start_streaming", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_CUR));
-//        std::cout << "exposition courant : " << aTime << std::endl;
-        
+               
         //copy frame to Vector Pictures
         cv::Mat mat(gHeight, gWidth, CV_8UC3, gFrame->data);
-        mat.copyTo(gData->pictures[imgIndex]);
+        mat.copyTo(gData.pictures[imgIndex]);
 
-        //std::cout <<"frame " << gFrame->sequence << " index " << imgIndex << " exp " << gData->exp[imgIndex];// << std::endl;
-   
         // change exposure time for next capture    
-        imgIndex = (imgIndex + 1) % gData->nbImg;
-        tryUvc("set_exp_abs", uvc_set_exposure_abs(gDevh, gData->exp[imgIndex]));
+        imgIndex = (imgIndex + 1) % gData.nbImg;
+        tryUvc("set_exp_abs", uvc_set_exposure_abs(gDevh, gData.exp[imgIndex]));
+
+        gChrono.restart();
+        gData.nbframe =0;
         
-        //std::cout << " Next " << imgIndex << " exp " << gData->exp[imgIndex] << std::endl;
-        gData->nbframe =0;
-        
-//        tryUvc("get_exposure_abs", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_INFO));
-//        std::cout << "exposition courant info: " << aTime << std::endl;
-       
-    } else gData->nbframe++ ;
+    } else gData.nbframe++ ;
     
 }
 
@@ -155,12 +145,13 @@ int main(int argc, char** argv) {
 
     cv::namedWindow("MonHDR");
     
-    Data data; 
-    data.init(pexpmin,pexpmax,pnb);
-    gData = &data;
+    //Data data; 
+    gData.init(pexpmin,pexpmax,pnb);
+    //gData = &data;
+    //imgIndex = data.nbImg-1;
     
     cv::Mat image(gHeight,gWidth,CV_8UC3,{255,255,255});
-    data.img = image;
+    gData.img = image;
     cv::imshow("MonHDR", image);
     cv::moveWindow("MonHDR", 0, 0);
     
@@ -176,35 +167,11 @@ int main(int argc, char** argv) {
     
     tryUvc("get_ctrl", uvc_get_stream_ctrl_format_size(gDevh, 
                 &(gCtrl), UVC_FRAME_FORMAT_ANY, gWidth, gHeight, 30));
-//    tryUvc("get_ctrl", uvc_get_stream_ctrl_format_size(gDevh, 
-//                &(gCtrl), UVC_FRAME_FORMAT_BGR, gWidth, gHeight, 30));
-
-        /** Get a negotiated streaming control block for some common parameters.
-        *  streaming
-        *
-        * [in] devh Device handle
-        * [in,out] ctrl Control block
-        * [in] format_class Type of streaming format
-        * [in] width Desired frame width
-        * [in] height Desired frame height
-        * [in] fps Frame rate, frames per second
-        */
     
     tryUvc("set_ae_mode", uvc_set_ae_mode(gDevh, 1)); // origin  1
-          /*~auto_exposure (int, default: 1)
-            * Cameras may support any of the following AE modes:
-            *  * UVC_AUTO_EXPOSURE_MODE_MANUAL (1) - manual exposure time, manual iris
-            *  * UVC_AUTO_EXPOSURE_MODE_AUTO (2) - auto exposure time, auto iris
-            *  * UVC_AUTO_EXPOSURE_MODE_SHUTTER_PRIORITY (4) - manual exposure time, auto iris
-            *  * UVC_AUTO_EXPOSURE_MODE_APERTURE_PRIORITY (8) - auto exposure time, manual iris
-            *  */
-    
-    tryUvc("set_exp_abs", uvc_set_exposure_abs(gDevh, data.expMin));
-         /*~exposure_absolute (double, default: 0.0)
-            * The `time` parameter should be provided in units of 0.0001 seconds (e.g., use the value 100
-            * for a 10ms exposure period). Auto exposure should be set to `manual` or `shutter_priority`
-            * before attempting to change this setting.*/
-    
+             
+    tryUvc("set_exp_abs", uvc_set_exposure_abs(gDevh, gData.expMin));
+             
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     //tryUvc("stream_open_ctrl", uvc_stream_open_ctrl(gDevh, &gStrmh, &gCtrl));
     
@@ -214,42 +181,17 @@ int main(int argc, char** argv) {
         std::cerr << "unable to allocate frame!\n";
         exit(-1);
     }
-    
-    //tryUvc("stream_start_iso", uvc_stream_start_iso(gStrmh, nullptr, nullptr));    
-    //tryUvc("stream_start", uvc_stream_start(gStrmh, captureFun, nullptr, 0));
-    
+    gChrono.restart();    
     //Start visualisation
     tryUvc("start_streaming", uvc_start_streaming(gDevh, 
-                &(gCtrl), captureFun, nullptr, 0));
+                &(gCtrl), captureFun, nullptr , 0)); //
     
         
-//    tryUvc("start_streaming", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_MIN));    
-//    std::cout << "exposition minimum : " << aTime << std::endl;
-//    
-//    tryUvc("start_streaming", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_MAX));
-//    std::cout << "exposition maximum : " << aTime << std::endl;
-//    
-//    tryUvc("start_streaming", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_DEF));
-//    std::cout << "exposition defaut : " << aTime << std::endl;
-//    
-//    tryUvc("start_streaming", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_CUR));
-//    std::cout << "exposition courant : " << aTime << std::endl;
-//    
-//    tryUvc("start_streaming", uvc_get_exposure_abs(gDevh, 
-//              &aTime , UVC_GET_INFO));
-//    std::cout << "exposition info : " << aTime << std::endl;
-        
-//    tryUvc("start_streaming", uvc_start_streaming(gDevh, 
-//                &(gCtrl), nullptr, nullptr, 0));
-    
+   
    
     try {
         
-       // int idx;
+        int idx;
 
         // terminate when window is closed
         while (cv::getWindowProperty("MonHDR", 0) != -1) {
@@ -264,93 +206,41 @@ int main(int argc, char** argv) {
                     std::cout << "fermeture" << std::endl;
                     break;
                 case 32: // terminate when <Space> is hit "space bar"
-                    cv::imshow("MonHDR", calculImageHdr(data.pictures,data.times)); 
+                    cv::imshow("MonHDR", calculImageHdr(gData.pictures,gData.times)); 
                     break;
                 case 48: // terminate when <0> 
-                    cv::imshow("MonHDR", data.pictures[0]); 
+                    cv::imshow("MonHDR", gData.pictures[0]); 
                     break;    
                 case 49: // terminate when <0> 
-                    cv::imshow("MonHDR", data.pictures[1]); 
+                    cv::imshow("MonHDR", gData.pictures[1]); 
                     break;
                 case 50: // terminate when <0> 
-                    if (data.nbImg > 2 ) {
-                    cv::imshow("MonHDR", data.pictures[2]); 
+                    if (gData.nbImg > 2 ) {
+                    cv::imshow("MonHDR", gData.pictures[2]); 
                     } 
                     break;
                 case 51: // terminate when <0> 
-                    if (data.nbImg > 3 ) {
-                    cv::imshow("MonHDR", data.pictures[3]); 
+                    if (gData.nbImg > 3 ) {
+                    cv::imshow("MonHDR", gData.pictures[3]); 
                     }
                     break;
                 case 52: // terminate when <0> 
-                    if (data.nbImg > 4 ) {
-                    cv::imshow("MonHDR", data.pictures[4]); 
+                    if (gData.nbImg > 4 ) {
+                    cv::imshow("MonHDR", gData.pictures[4]); 
                     } 
                     break;
                 default:
                     
-////                    std::cout  << "while"<< std::endl;
-//                    for (idx = 0; idx < data.nbImg; idx++) {
-//                        //charge les images
-//                        
-////                        std::cout << "idx"  << idx << std::endl;
-//                       // tryUvc("set_exp_abs", uvc_set_exposure_abs(gDevh, data.exp[idx]*10));
-//                       // tryUvc("stream_get_frame", uvc_stream_get_frame(gStrmh, &gFrame, 0));
-//                        
-//                        //tryUvc("any2bgr", uvc_any2bgr(gFrame, aFrame));
-//                       //cv::Mat mat(gHeight, gWidth, CV_8UC3, gFrame->data);
-////                        std::cout << "size" << gFrame->data_bytes << " temps :" << gFrame->capture_time.tv_usec << " " << gFrame->capture_time.tv_sec << std::endl;
-////                        cv::Mat mat(gFrame->height, gFrame->width, gFrame->frame_format, gFrame->data);
-//                        
-////                        printImageStats(mat);
-////                        data.pictures[idx] = mat.clone();
-////                        cv::imshow("img"+std::to_string(idx), data.pictures[idx]);
-//                    }
-
-//                   
-                    // process new frame
-                    //cv::Mat mat(gHeight, gWidth, CV_8UC3, gFrame->data);
-                    
-                    //cv::imshow("img"+std::to_string(imgIndex), data.pictures[imgIndex]);
-                    
-                    
-                   // std::lock_guard<std::mutex> lock(gMutex);  
-                    
-                    //idx =  ((imgIndex-1)+data.nbImg) % data.nbImg;//(data.nbImg-1 - imgIndex);
-                    
-                    
-//                    idx =  imgIndex % data.nbImg;
-//                    std::cout << "bp idx " << idx <<std::endl;
-//                    cv::imshow("img"+std::to_string(idx)+"_"+
-//                               std::to_string(data.exp[idx]), data.pictures[idx]);
-                    
-                    for (int i = 0; i < data.nbImg; i++) {
-                        cv::imshow("img"+std::to_string(i)+"_"+std::to_string(data.exp[i]),data.pictures[i]);
-                    }
-//
-//                    
-//                    cv::imshow("img0_"+std::to_string(data.exp[0]),data.pictures[0]);
-//                    cv::imshow("img1_"+std::to_string(data.exp[1]),data.pictures[1]);
-//                    cv::imshow("img2_"+std::to_string(data.exp[2]),data.pictures[2]);
-//                   
-//                    
-//                    
-//                    //data.img = mat;     
-//                                        
-//                    data.pictures[imgIndex] = mat.clone();
-//                    cv::imshow("MonHDR", data.img);
-//                    cv::imshow("img"+std::to_string(imgIndex), data.pictures[imgIndex]);
-//                    std::cout << imgIndex << " exp" << data.exp[imgIndex] << std::endl;
-//                    std::cout << "Int" << cptInt << " Fun" << cptFun << std::endl;
-                    //cv::imshow("img"+std::to_string(imgIndex), data.pictures[imgIndex]);
+                     if (gChrono.elapsedRunning()*10 > gData.exp[imgIndex] )  {
+                        cv::imshow("img"+std::to_string(imgIndex)+"_"+std::to_string(gData.exp[imgIndex]),gData.pictures[imgIndex]); 
+                     }
+                     //  std::cout << imgIndex << " "  << " elapsed " << gChrono.elapsedRunning() << std::endl;
             }
-//            cptInt++;
         }
     }
     catch (cv::Exception&) {
     }
-    
-    
+       
     return 0;
 }
 
@@ -368,7 +258,7 @@ cv::Mat calculImageHdr(std::vector<cv::Mat> _images,
     }
     
     
-    gData->printTimes();
+    gData.printTimes();
     
     
     cv::Mat reponse;    
